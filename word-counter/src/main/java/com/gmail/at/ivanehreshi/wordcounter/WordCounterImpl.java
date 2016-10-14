da73b16fed24548aa36f2228da1067ec2e18a8ed
ivaneh <ivanehreshi@gmail.com>
2016-10-14 17:39:47 +0300

    Implement WordCounterImpl

--- word-counter/src/main/java/com/gmail/at/ivanehreshi/wordcounter/WordCounterImpl.java
+++ word-counter/src/main/java/com/gmail/at/ivanehreshi/wordcounter/WordCounterImpl.java
@@ -1,4 +1,33 @@
 package com.gmail.at.ivanehreshi.wordcounter;
 
-public class WordCounterImpl {
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCounterImpl implements WordCounter{
+    private Map<String, Integer> wordsMap;
+
+    public WordCounterImpl() {
+        wordsMap = new HashMap<>();
+    }
+
+    @Override
+    public int passWord(String word) {
+        Integer wordCount = wordsMap.getOrDefault(word, 0);
+        ++wordCount;
+        wordsMap.put(word, wordCount);
+        return wordCount;
+    }
+
+    @Override
+    public int count(String word) {
+        return wordsMap.getOrDefault(word, 0);
+    }
+
+    protected Map<String, Integer> getWordsMap() {
+        return wordsMap;
+    }
+
+    protected void setWordsMap(Map<String, Integer> wordsMap) {
+        this.wordsMap = wordsMap;
+    }
 }
