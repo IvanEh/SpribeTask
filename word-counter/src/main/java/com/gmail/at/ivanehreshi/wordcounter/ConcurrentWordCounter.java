781e4396d9874965435cfaff846eda557630f4a3
ivaneh <ivanehreshi@gmail.com>
2016-10-14 19:02:59 +0300

    Implement ConcurrentWordCounter

---
+++ word-counter/src/main/java/com/gmail/at/ivanehreshi/wordcounter/ConcurrentWordCounter.java
@@ -0,0 +1,32 @@
+package com.gmail.at.ivanehreshi.wordcounter;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ConcurrentWordCounter extends WordCounterImpl{
+    protected final ReadWriteLock rwLock;
+
+    public ConcurrentWordCounter() {
+        super();
+
+        rwLock = new ReentrantReadWriteLock();
+    }
+
+    @Override
+    public int count(String word) {
+        int ret;
+        rwLock.readLock().lock();
+        ret = super.count(word);
+        rwLock.readLock().unlock();
+        return ret;
+    }
+
+    @Override
+    public int passWord(String word) {
+        int ret;
+        rwLock.writeLock().lock();
+        ret = super.passWord(word);
+        rwLock.writeLock().unlock();
+        return ret;
+    }
+}
