2d8df8cf5a18f75a1c9c3ae219c24f331d2cc6b2
ivaneh <ivanehreshi@gmail.com>
2016-10-14 17:04:51 +0300

    Make changes in WordCounter.java interface

--- word-counter/src/main/java/com/gmail/at/ivanehreshi/wordcounter/WordCounter.java
+++ word-counter/src/main/java/com/gmail/at/ivanehreshi/wordcounter/WordCounter.java
@@ -7,14 +7,23 @@ import java.util.stream.Collectors;
 public interface WordCounter {
     int passWord(String word);
 
-    default List<Integer> passWords(String words) {
-        return passWords(words.split(" "));
+    default List<Integer> passWordsAndCount(String words) {
+        return passWordsAndCount(words.split(" "));
     }
 
-    default List<Integer> passWords(String... words) {
+    default List<Integer> passWordsAndCount(String... words) {
         return Arrays.stream(words)
                      .map(this::passWord)
                      .collect(Collectors.toList());
+    }
+
+    default void passWords(String... words) {
+        Arrays.stream(words)
+                     .map(this::passWord);
+    }
+
+    default void passWords(String words) {
+        passWords(words.split(" "));
     }
 
     int count(String word);
