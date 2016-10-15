43497f21481ed9ffcc0c03152529ae39ab680171
ivaneh <ivanehreshi@gmail.com>
2016-10-15 18:32:15 +0300

    Implement OpponentFinderImpl

--- find-opponent/src/main/java/com/gmail/at/ivanehreshi/findopponent/domain/OpponentFinderImpl.java
+++ find-opponent/src/main/java/com/gmail/at/ivanehreshi/findopponent/domain/OpponentFinderImpl.java
@@ -1,49 +1,136 @@
 package com.gmail.at.ivanehreshi.findopponent.domain;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class OpponentFinderImpl implements OpponentFinder {
-    private List<Player> readyPlayers;
+    private TreeMap<Player, List<Player>> readyPlayers;  // Actually MultiSet
+    private int size;
+
+    public OpponentFinderImpl() {
+        readyPlayers = new TreeMap<>(new PlayerComparator());
+        size = 0;
+    }
 
     @Override
     public void addReadyPlayer(Player player) {
-        readyPlayers.add(player);
+        if(player == null)
+            return;
+
+        List<Player> players = readyPlayers.get(player);
+
+        if(players != null) {
+            players.add(player);
+        } else {
+            players = new ArrayList<>();
+            players.add(player);
+            readyPlayers.put(player, players);
+        }
+
+        ++this.size;
     }
 
     @Override
     public Player findOpponent(Player player) throws IllegalArgumentException{
-        return null;
+        boolean removed = this.removePlayer(player);
+
+
+        if(!removed) {
+            throw new IllegalArgumentException("Cannot find opponent for busy Player");
+        }
+
+        if(readyPlayers.isEmpty()) {
+            addReadyPlayer(player);
+            return null;
+        }
+
+        Player lowerPlayer = anyPlayer(readyPlayers.floorEntry(player));
+        Player higherPlayer = anyPlayer(readyPlayers.ceilingEntry(player));
+
+        Player nearestPlayer = findNearest(player, lowerPlayer, higherPlayer);
+        removePlayer(nearestPlayer);
+        return nearestPlayer;
+    }
+
+    private Player findNearest(Player player, Player lowerPlayer, Player higherPlayer) {
+        if(lowerPlayer == null) {
+            return higherPlayer;
+        }
+
+        if(higherPlayer == null) {
+            return lowerPlayer;
+        }
+
+        double lowDelta = Math.abs(player.getRating() - lowerPlayer.getRating());
+        double hiDelta = Math.abs(player.getRating() - higherPlayer.getRating());
+
+        if(lowDelta >= hiDelta) {
+            return lowerPlayer;
+        } else {
+            return higherPlayer;
+        }
     }
 
     @Override
     public int countReady() {
-        return 0;
+        return size;
     }
 
     @Override
     public void clearReadyPlayers() {
-
+        readyPlayers.clear();
+        size = 0;
     }
 
     @Override
-    public int removePlayer(Player player) {
-        return 0;
+    public boolean removePlayer(Player player) {
+        List<Player> players = readyPlayers.get(player);
+
+        if(players == null) {
+            return false;
+        }
+
+        boolean removed = players.remove(player);
+        if(removed) {
+            this.size--;
+
+            if(players.isEmpty()) {
+                readyPlayers.remove(player);
+            }
+        }
+
+        return removed;
     }
 
     @Override
     public List<Player> playersAsList() {
-        return null;
+        List<Player> players = readyPlayers.entrySet().stream()
+                .map(Map.Entry::getValue)
+                .flatMap((playerList) -> playerList.stream())
+                .collect(Collectors.toList());
+
+        return players;
     }
 
-    public List<Player> getReadyPlayers() {
-        return readyPlayers;
+    private static class PlayerComparator implements Comparator<Player> {
+        @Override
+        public int compare(Player p1, Player p2) {
+            double delta = p1.getRating() - p2.getRating();
+            if(delta == 0 && p1.getName() != null) {
+                return p1.getName().compareTo(p2.getName());
+            }
+
+            return (int) Math.signum(delta);
+        }
     }
 
-    public void setReadyPlayers(List<Player> readyPlayers) {
-        this.readyPlayers = readyPlayers;
+    private static Player anyPlayer(Map.Entry<Player, List<Player>> entry) {
+        if (entry == null) {
+            return null;
+        }
+
+        return entry.getValue().get(0);
     }
-
-
 
 
 }
