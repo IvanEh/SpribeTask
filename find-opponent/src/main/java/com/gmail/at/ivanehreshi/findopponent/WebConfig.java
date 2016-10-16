5b56d54bb148eb9645ded7caf2d270759247e8c9
ivaneh <ivanehreshi@gmail.com>
2016-10-16 12:26:15 +0300

    Add static resources mapping

--- find-opponent/src/main/java/com/gmail/at/ivanehreshi/findopponent/WebConfig.java
+++ find-opponent/src/main/java/com/gmail/at/ivanehreshi/findopponent/WebConfig.java
@@ -3,10 +3,16 @@ package com.gmail.at.ivanehreshi.findopponent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.gmail.at.ivanehreshi.findopponent.controller")
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("**/*.html", "**/*.css", "**/*.js")
+                .addResourceLocations("/resources/");
+    }
 }
